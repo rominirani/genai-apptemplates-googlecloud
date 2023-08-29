adeb3cd3c56197591c5b0d1f810e922aa476858f
Giuseppe Cofano <cofano@google.com>
2023-08-29 11:03:03 +0000

    Adding logger to the Flask Cloud Run example

--- chat-flask-cloudrun/app.py
+++ chat-flask-cloudrun/app.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template, request, jsonify
 import vertexai
 from vertexai.preview.language_models import ChatModel
 import os
+import google.cloud.logging
 
 app = Flask(__name__)
 PROJECT_ID = os.environ.get('GCP_PROJECT') #Your Google Cloud Project ID
 LOCATION = os.environ.get('GCP_REGION')   #Your Google Cloud Project Region
+
+client = google.cloud.logging.Client(project=PROJECT_ID)
+client.setup_logging()
+
+LOG_NAME = "flask-app-internal-logs"
+logger = client.logger(LOG_NAME)
+
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
 def create_session():
@@ -35,7 +43,9 @@ def vertex_palm():
         user_input = request.args.get('user_input')
     else:
         user_input = request.form['user_input']
+    logger.log(f"Starting chat session...")
     chat_model = create_session()
+    logger.log(f"Chat Session created")
     content = response(chat_model,user_input)
     return jsonify(content=content)
 
