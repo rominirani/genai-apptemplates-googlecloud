366a6452d333f539b1043f74cf082eb5a502dbfd
destrex271 <destrex271@gmail.com>
2023-09-04 15:25:44 +0530

    Updated scripts: Automated log analysis action for GitHub Action pipelines

--- palm-api-apps/palm_analysis_action/entrypoint.sh
+++ palm-api-apps/palm_analysis_action/entrypoint.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
 
-file_path=$1
-api_key=$2
+# Update script
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y python3 python3-pip
-# pip install -r $GITHUB_PATH/requirements.txt
-#
-# touch analysis.txt
-# result=$(python3 $GITHUB_PATH/main.py $1 $2)
-#
-# touch result.txt
-# echo "$result" >> result.txt
-# cat result.txt
-
-# exit $?
-
