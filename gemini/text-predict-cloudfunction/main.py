606076337b8d8eb4a3cf286ffd9cd919397f7768
Romin Irani <1614870+rominirani@users.noreply.github.com>
2024-02-01 04:49:26 +0530

    Create main.py

---
+++ gemini/text-predict-cloudfunction/main.py
@@ -0,0 +1,45 @@
+import json
+import os
+
+from google.cloud import logging
+import functions_framework
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part
+
+PROJECT_ID = os.environ.get("GCP_PROJECT", "-")
+LOCATION = os.environ.get("GCP_REGION", "-")
+
+client = logging.Client(project=PROJECT_ID)
+client.setup_logging()
+
+LOG_NAME = "predictText-cloudfunction-log"
+logger = client.logger(LOG_NAME)
+
+@functions_framework.http
+def predictText(request):
+    request_json = request.get_json(silent=True)
+
+    if request_json and "prompt" in request_json:
+        prompt = request_json["prompt"]
+        logger.log(f"Received request for prompt: {prompt}")
+        vertexai.init(project=PROJECT_ID, location=LOCATION)
+        model = GenerativeModel("gemini-pro")
+
+        responses = model.generate_content(
+            contents=prompt,
+            generation_config={
+                "max_output_tokens": 2048,
+                "temperature": 0.4,
+                "top_p": 1,
+                "top_k": 32
+            },
+        stream=True,
+        )
+
+        prompt_response = ""
+        for response in responses:
+            prompt_response.join(response.text)
+    else:
+        prompt_response = "No prompt provided."
+
+    return json.dumps({"response_text": prompt_response})
